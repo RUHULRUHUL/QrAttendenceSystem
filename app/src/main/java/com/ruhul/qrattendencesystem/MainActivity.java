063f145552da990423031bf49f6d0997fc34a059
RuhulRuhul <ruhulamin122017@gmail.com>
2021-04-11 00:27:06 +0600

    project create -4/11/2021 by ruhul 12.26

---
+++ app/src/main/java/com/ruhul/qrattendencesystem/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ruhul.qrattendencesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //project create by ruhul-----------------
+    }
+}
