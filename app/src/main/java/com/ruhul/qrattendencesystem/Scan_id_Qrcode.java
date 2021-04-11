fa6fd0831c2134361e18b1ad87ff4e4a510a1a0f
fayej <fayej019fa@gmail.com>
2021-04-12 00:09:39 +0600

    fayej creae-scan class--

---
+++ app/src/main/java/com/ruhul/qrattendencesystem/Scan_id_Qrcode.java
@@ -0,0 +1,15 @@
+package com.ruhul.qrattendencesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Scan_id_Qrcode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_id__qrcode);
+        //fayej create this class-------------
+    }
+}
