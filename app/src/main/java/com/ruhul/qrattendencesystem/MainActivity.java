2c39bb73afce45a737ae8eea14b2039b1f2fb9e4
RuhulRuhul <ruhulamin122017@gmail.com>
2021-04-15 23:57:34 +0600

    project create -4/11/2021 by ruhul 12.26 library add-codescanner and dexter-4/12/21 codescanner add cheack it

--- app/src/main/java/com/ruhul/qrattendencesystem/MainActivity.java
+++ app/src/main/java/com/ruhul/qrattendencesystem/MainActivity.java
@@ -2,15 +2,32 @@ package com.ruhul.qrattendencesystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.budiyev.android.codescanner.CodeScanner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CodeScanner mCodeScanner;
+    private Button pushscan_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //project create by ruhul-----------------
+        pushscan_btn=findViewById(R.id.scan_id);
+
+        pushscan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Scan_id_Qrcode.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/ruhul/qrattendencesystem/Scan_id_Qrcode.java
+++ app/src/main/java/com/ruhul/qrattendencesystem/Scan_id_Qrcode.java
@@ -1,14 +1,32 @@
 package com.ruhul.qrattendencesystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.PermissionRequest;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.List;
 
 public class Scan_id_Qrcode extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+    private CodeScannerView scannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +34,81 @@ public class Scan_id_Qrcode extends AppCompatActivity {
         setContentView(R.layout.activity_scan_id__qrcode);
         //fayej create this class-------------
 
+         scannerView= findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
 
+
+
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+
+
+                        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+                            @Override
+                            public void onDecoded(@NonNull final Result result) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(Scan_id_Qrcode.this, result.getText(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        });
+
+                    }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response)
+                    {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(com.karumi.dexter.listener.PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                        permissionToken.continuePermissionRequest();
+                    }
+
+                }).check();
+
+
+        if (ContextCompat.checkSelfPermission(Scan_id_Qrcode.this,Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED)
+        {
+            scannerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mCodeScanner.startPreview();
+                }
+            });
+
+        }
+
+
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(Scan_id_Qrcode.this,Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED)
+        {
+            mCodeScanner.startPreview();
+
+        }
+        else {
+            Toast.makeText(this, "permission need,allow permission", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
     }
 }
